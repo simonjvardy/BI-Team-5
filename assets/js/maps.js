1d6860f8425d7b8ecba5979c4f37c8aaeff7f192
AlexNexton <cianoc13@gmail.com>
2020-07-07 19:08:28 +0000

    contact section: styled and repsonsive

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,104 +1,99 @@
- 
- let map, infoWindow; //variables for Geolocation 
+function initMap() { // Google maps function and theme
 
- function initMap(){ // Google maps function and theme
+ let coffeeShop = { lat: 53.346925, lng: -6.262882 };
 
+    let map = new google.maps.Map(document.getElementById("map"), {
 
- 
-     let coffeeShop = {lat: 53.346925, lng: -6.262882};
+        zoom: 12,
 
-          let map = new google.maps.Map(document.getElementById("map"),{
-              
-              zoom: 12,
-              
-              center: coffeeShop,
-              
-               styles: [
-            {elementType: 'geometry', stylers: [{color: '#615550'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        center: coffeeShop,
+
+        styles: [
+            { elementType: 'geometry', stylers: [{ color: '#615550' }] },
+            { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
             {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
             },
             {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
             },
             {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{ color: '#263c3f' }]
             },
             {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#6b9a76' }]
             },
             {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{ color: '#38414e' }]
             },
             {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#212a37' }]
             },
             {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#9ca5b3' }]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{ color: '#746855' }]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#1f2835' }]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#f3d19c' }]
             },
             {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{ color: '#2f3948' }]
             },
             {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
             },
             {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{ color: '#17263c' }]
             },
             {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#515c6d' }]
             },
             {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{ color: '#17263c' }]
             }
-          ]
-             
-          });
-          
-    /* Code for info window on coffeeShop */
+        ]
+
+    });
+
+    /* Code for info window on map on index.html page */
     let contentString =
-    `
+        `
     <div id="content"> 
     <div id="siteNotice"> 
     </div> 
@@ -115,19 +110,17 @@
     </div> 
     </div>`;
     
+    let infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
 
-  let infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+    var marker = new google.maps.Marker({
+        position: coffeeShop,
+        map: map,
+        title: "CaféJava"
+    });
+    marker.addListener("mouseover", function () {
+        infowindow.open(map, marker);
+    });
 
-  var marker = new google.maps.Marker({
-    position: coffeeShop,
-    map: map,
-    title: "CaféJava"
-  });
-  marker.addListener("mouseover", function() {
-    infowindow.open(map, marker);
-  });
-  
- }
-     
+}
