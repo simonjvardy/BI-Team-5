9f0b54c35cb56706e5e226040ae39f72cf0a6f9f
AlexNexton <cianoc13@gmail.com>
2020-07-07 18:12:45 +0000

    js: added mouseover event - changed map zoom

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,7 +9,7 @@
 
           let map = new google.maps.Map(document.getElementById("map"),{
               
-              zoom: 10,
+              zoom: 12,
               
               center: coffeeShop,
               
@@ -125,41 +125,9 @@
     map: map,
     title: "CaféJava"
   });
-  marker.addListener("click", function() {
+  marker.addListener("mouseover", function() {
     infowindow.open(map, marker);
   });
   
-
- /*  var marker = new google.maps.Marker({position: coffeeShop, map: map});
-        infoWindow = new google.maps.InfoWindow;
-
-        //  Geolocation of user
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            let pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map); */
-  
-     }
+ }
      
